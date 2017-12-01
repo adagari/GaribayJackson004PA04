921e8dccfc13b0a892d9cdde5c126bcae1e00fa3
adagari <Adam.garibay@gmail.com>
2017-12-01 14:04:03 -0600

    Update GaribayJackson004PA04.java

--- GaribayJackson004PA04.java
+++ GaribayJackson004PA04.java
@@ -21,13 +21,13 @@ public class GaribayJackson004PA04 {
 
 /**
  * 
- Do you want to begin processing cable  bills? Enter 'Y' or 'N':   y
+Do you want to begin processing cable  bills? Enter 'Y' or 'N':  y
  
- How many bills are there to process?   3
+ How many bills are there to process?  3
  
  WELCOME TO SA CABLE 
  
- Please enter customer 1's name:   gary dempsey
+ Please enter customer 1's name:  gary jones
  
  SA CABLE - SUBSCRIPTION PACKAGES - INCLUDES INTERNET SERVICE 
  
@@ -35,15 +35,15 @@ public class GaribayJackson004PA04 {
  2. Deluxe: Local, major TV, cable & 100 other channels                 75.00 
  3. Premium: Deluxe package plus HBO, on-demand & 300 other channels   110.00 
  
- Select the cable subscription package for customer 1:   3
+ Select the cable subscription package for customer 1:  3
  
  SA CABLE - MOVIES 
  
- Enter the number of Movies-On-Demand-HD purchases for 1:   25
+ Enter the number of Movies-On-Demand-HD purchases for customer 1:  25
  
  WELCOME TO SA CABLE 
  
- Please enter customer 2's name:   rex dempsey
+ Please enter customer 2's name:  rex dempsey
  
  SA CABLE - SUBSCRIPTION PACKAGES - INCLUDES INTERNET SERVICE 
  
@@ -51,7 +51,7 @@ public class GaribayJackson004PA04 {
  2. Deluxe: Local, major TV, cable & 100 other channels                 75.00 
  3. Premium: Deluxe package plus HBO, on-demand & 300 other channels   110.00 
  
- Select the cable subscription package for customer 2:   20
+ Select the cable subscription package for customer 2:  20
  
  Invalid Selection! 
  
@@ -61,15 +61,15 @@ public class GaribayJackson004PA04 {
  2. Deluxe: Local, major TV, cable & 100 other channels                 75.00 
  3. Premium: Deluxe package plus HBO, on-demand & 300 other channels   110.00 
  
- Select the cable subscription package for customer 2:   2
+ Select the cable subscription package for customer 2:  2
  
  SA CABLE - MOVIES 
  
- Enter the number of Movies-On-Demand-HD purchases for 2:   [DrJava Input Box]
+ Enter the number of Movies-On-Demand-HD purchases for customer 2:  25
  
  WELCOME TO SA CABLE 
  
- Please enter customer 3's name:   25
+ Please enter customer 3's name:  maggie jones
  
  SA CABLE - SUBSCRIPTION PACKAGES - INCLUDES INTERNET SERVICE 
  
@@ -77,34 +77,27 @@ public class GaribayJackson004PA04 {
  2. Deluxe: Local, major TV, cable & 100 other channels                 75.00 
  3. Premium: Deluxe package plus HBO, on-demand & 300 other channels   110.00 
  
- Select the cable subscription package for customer 3:   maggie jones
+ Select the cable subscription package for customer 3:  l
  
- You must only enter an integer:  
+ You must only enter an integer:   
  
  1. Basic: Local & major TV network channels                          $ 35.00 
  2. Deluxe: Local, major TV, cable & 100 other channels                 75.00 
  3. Premium: Deluxe package plus HBO, on-demand & 300 other channels   110.00 
  
- Select the cable subscription package for customer 3:   l
- 
- You must only enter an integer:
- 
- 1. Basic: Local & major TV network channels                          $ 35.00 
- 2. Deluxe: Local, major TV, cable & 100 other channels                 75.00 
- 3. Premium: Deluxe package plus HBO, on-demand & 300 other channels   110.00
- 
- Select the cable subscription package for customer 3:   1
+ Select the cable subscription package for customer 3:  1
  
  SA CABLE - MOVIES 
  
- Enter the number of Movies-On-Demand-HD purchases for 3:   0
+ Enter the number of Movies-On-Demand-HD purchases for customer 3:  O
  
- You must only enter an integer:  
+ You must only enter an integer:   
  
- Enter the number of Movies-On-Demand-HD purchases:   0
+ Enter the number of Movies-On-Demand-HD purchases:  0
  
- SA CABLE CHARGES AS OF 11/22/17 
- Customer: GARY MAYES  
+ 
+ SA CABLE CHARGES AS OF 12/01/17 
+ Customer: GARY JONES 
  
  Cable Service:                    $    110.00 
  Movies-On-Demand-HD:                   175.00 
@@ -114,7 +107,7 @@ public class GaribayJackson004PA04 {
  Thank you for being a valued SA Cable Customer! 
  
  
- SA CABLE CHARGES AS OF 11/22/17 
+ SA CABLE CHARGES AS OF 12/01/17 
  Customer: REX DEMPSEY 
  
  Cable Service:                    $     75.00 
@@ -125,7 +118,7 @@ public class GaribayJackson004PA04 {
  Thank you for being a valued SA Cable Customer! 
  
  
- SA CABLE CHARGES AS OF 11/22/17 
+ SA CABLE CHARGES AS OF 12/01/17 
  Customer: MAGGIE JONES 
  
  Cable Service:                    $     35.00 
@@ -133,5 +126,5 @@ public class GaribayJackson004PA04 {
  
  TOTAL DUE:                        $     35.00 
  
- Thank you for being a valued SA Cable Customer! 
+ Thank you for being a valued SA Cable Customer!
  */
